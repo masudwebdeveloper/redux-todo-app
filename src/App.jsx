42bda7269456b74cdf913aefd8fcc26d53c2c609
masudrana5 <abdullainfo30@gmail.com>
2023-04-09 00:13:36 +0600

    simple todo list is create is done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navber from "./components/Navber";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+      <Navber />
+
+      <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+        <Header />
+
+        <hr className="mt-4" />
+
+        <TodoList />
+
+        <hr className="mt-4" />
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
