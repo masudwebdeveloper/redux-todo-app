9dec270a3df7058f1fabb58b42aa81fa7385a5e9
masudrana5 <abdullainfo30@gmail.com>
2023-04-09 01:50:35 +0600

    todos reducer write is done

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+const initialState = [
+    {
+        id: 1,
+        text: "learn English with LTDEZ",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Learn web development with learn with sumit",
+        completed: true
+    }
+];
+export default initialState;
